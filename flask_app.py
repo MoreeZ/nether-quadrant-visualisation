0f268a547902e76d5d16558c6079247a99855f7e
Oskar Mroz <moreez@protonmail.com>
2024-10-20 14:43:12 +0100

    renamed fortress to quadrants

--- flask_app.py
+++ flask_app.py
@@ -15,15 +15,15 @@ def quadrants():
         return {"error": "Please provide both x_pos and z_pos"}, 400
 
     try:
-        # Run the fortress.py file with x_pos and z_pos as arguments
-        subprocess.run(['python', 'fortress.py', x_pos, z_pos, output_file], check=True)
+        # Run the quadrants.py file with x_pos and z_pos as arguments
+        subprocess.run(['python', 'quadrants.py', x_pos, z_pos, output_file], check=True)
 
         # Return the generated image
         response = send_file(output_file, mimetype='image/png')
         os.remove(output_file)
         return response;
     except subprocess.CalledProcessError as e:
-        return {"error": "Failed to generate image from fortress.py", "details": str(e)}, 500
+        return {"error": "Failed to generate image from quadrants.py", "details": str(e)}, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
