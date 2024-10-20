7ffa83ab7eb1d28aea2a71fae401f756d038be55
Oskar Mroz <moreez@protonmail.com>
2024-10-20 16:00:12 +0100

    changed cors code

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from quadrants import locate_quadrants # Import the function from quadrants.py
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/quadrants/*": {"origins": "*"}})
+CORS(app)
 
 @app.route('/quadrants', methods=['GET'])
 def quadrants():
